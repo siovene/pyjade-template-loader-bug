c8b869692dd14c7dfabb76026eeb59dd93113a5b
Salvatore Iovene <salvatore@iovene.com>
2012-09-12 10:06:20 +0300

    Initial commit.

---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from django.views.generic.base import TemplateView
+
+class MyView(TemplateView):
+  template_name = 'my_template.html'
+
+  def get_context_data(self):
+    return {
+      'exclude': '',
+      'names': ['Alice', 'Bob'],
+    }
